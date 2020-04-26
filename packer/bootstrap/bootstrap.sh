63d033be55b62d7a1472e0e0104fe231dfaaec17
yogeshvk1209 <yogeshvk1209@gmail.com>
2020-04-26 08:13:20 +0000

    Changing wget command

--- packer/bootstrap/bootstrap.sh
+++ packer/bootstrap/bootstrap.sh
@@ -18,17 +18,17 @@ chown -R 1000:1000 /var/run/docker.sock
 ## Creating Docker images for master and slave jenkins
 echo "Downloading Files from git"
 cd /opt/bootstrap/
-wget --no-check-certificate --content-disposition -o /opt/bootstrap/master https://raw.githubusercontent.com/yogeshvk1209/jenkins_setup/master/packer/bootstrap/master/Dockerfile --output-document=Dockerfile
+wget --no-check-certificate --content-disposition -O /opt/bootstrap/master/Dockerfile  https://raw.githubusercontent.com/yogeshvk1209/jenkins_setup/master/packer/bootstrap/master/Dockerfile
 wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/yogeshvk1209/jenkins_setup/master/packer/bootstrap/plugins.txt --output-document=plugins.txt
 wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/yogeshvk1209/jenkins_setup/master/packer/bootstrap/security.groovy --output-document=security.groovy
-wget --no-check-certificate --content-disposition -o /opt/bootstrap/slave https://raw.githubusercontent.com/yogeshvk1209/jenkins_setup/master/packer/bootstrap/slave/Dockerfile --output-document=Dockerfile
+wget --no-check-certificate --content-disposition -O /opt/bootstrap/slave/Dockerfile  https://raw.githubusercontent.com/yogeshvk1209/jenkins_setup/master/packer/bootstrap/slave/Dockerfile
 
 echo "Building jenkins master Docker Image"
-docker build -t jenkins-demo /opt/bootstrap/master/
+docker build -t jenkins-master /opt/bootstrap/master/
 
 sleep 10
 echo "Running Jenkins Docker container"
-docker run --name jenkins-demo -d -p 8080:8080 -p 9080:9080 -v jenkins_data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins-demo
+docker run --name jenkins-master -d -p 8080:8080 -p 9080:9080 -v jenkins_data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins-master
 
 echo "Building jenkins slave Docker Image"
-docker build -t jenkins-demo /opt/bootstrap/slave/
+docker build -t jenkins-slave /opt/bootstrap/slave/
